aef56ad4be5f91cc44b9b436b2ec020b6be965b8
Levy Silva <levysillva@gmail.com>
2024-10-05 15:58:03 -0300

    Add Responser

---
+++ src/helpers/Responser.js
@@ -0,0 +1,24 @@
+class Responser {
+  constructor(res) {
+    this.res = res;
+    this.statusCode = 200;
+    this.headers = { "Content-Type": "application/json" };
+  }
+
+  status(code) {
+    this.statusCode = code;
+    return this;
+  }
+
+  setHeader(name, value) {
+    this.headers[name] = value;
+  }
+
+  json(data) {
+    this.res.writeHead(this.status, this.headers);
+    this.res.end(JSON.stringify(data));
+    return this;
+  }
+}
+
+module.exports = Responser;
