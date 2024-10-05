4e9c90d277e6cee76462d7be68d951d5022220e1
Levy Silva <levysillva@gmail.com>
2024-10-05 16:32:53 -0300

    Add User.js as an entity of the application

---
+++ src/entity/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ nome, idade, sexo, habilitado }) {
+    this.nome = nome;
+    this.idade = idade;
+    this.sexo = sexo;
+    this.habilitado = habilitado;
+  }
+}
+
+module.exports = User;
