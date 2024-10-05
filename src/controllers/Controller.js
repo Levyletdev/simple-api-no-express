4941b24837b33526d8a15e9a04cc2460a285eef1
Levy Silva <levysillva@gmail.com>
2024-10-05 19:04:19 -0300

    Update Controller.js with getById method

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,4 +1,5 @@
 const Responser = require("../helpers/Responser");
+const getId = require("../utils/getId");
 
 class Controller {
   constructor(entityUseCase) {
@@ -9,6 +10,20 @@ class Controller {
     try {
       const recordList = await this.useCase.getAll();
       return new Responser(res).status(200).json(recordList);
+    } catch (error) {
+      return new Responser(res).status(500).json({ message: error.message });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const id = getId(req);
+      const record = await this.useCase.getById(Number(id));
+      if (!id)
+        return new Responser(res).status(400).json({
+          message: "ID do registro não localizado",
+        });
+      return new Responser(res).status(200).json(record);
     } catch (error) {
       return new Responser(res).status(500).json({ message: error.message });
     }
