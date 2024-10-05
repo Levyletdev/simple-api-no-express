a03b847809b7a0b4800287d80ac2b74abe1856ba
Levy Silva <levysillva@gmail.com>
2024-10-05 18:23:18 -0300

    Add UserUseCase.js

--- src/use-cases/UserUseCase.js
+++ src/use-cases/UserUseCase.js
@@ -1,14 +1,18 @@
+const User = require("../entity/User.js");
+
 class UserUseCase {
   constructor(userRepository) {
     this.userRepository = userRepository;
   }
 
-  async getAll() {
-    return this.userRepository.getAll()
+  async getAllUsers() {
+    return this.userRepository.getAll();
   }
 
-  async create(dto) {
-    
-    return this.userRepository.create()
+  async createUser(dto) {
+    const user = new User(dto);
+    return this.userRepository.create(user);
   }
 }
+
+module.exports = UserUseCase;
