628fdf79801c74474c42164ce1fd09a146f8146b
Levy Silva <levysillva@gmail.com>
2024-10-05 16:00:06 -0300

    Add routeRoutes.js

---
+++ src/router/rootRoute.js
@@ -0,0 +1,7 @@
+const Responser = require("../helpers/Responser");
+
+function rootRoute(req, res) {
+  return new Responser(res).status(200).json({ message: "Rota raiz escutando" });
+}
+
+module.exports = rootRoute;
