7b5744b35132ca91a0f7a30649725f63cec2a62e
Levy Silva <levysillva@gmail.com>
2024-10-05 16:03:23 -0300

    Update app.js with router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const http = require("http");
+const router = require("./router/router.js");
 
-const app = http.createServer((req, res) => {});
+const app = http.createServer((req, res) => {
+  router(req, res);
+});
 
 module.exports = app;
