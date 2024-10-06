b7791a92a5fe4bb65b9150094e4ac114c1379ad6
Levy Silva <levysillva@gmail.com>
2024-10-05 21:41:56 -0300

    Add userRoutes import to router.js to handle ./users endpoint

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,10 @@
 const rootRoute = require("./rootRoute.js");
+const userRoutes = require("./userRoutes.js");
 
 function router(req, res) {
   const { url } = req;
   if (url === "/") return rootRoute(req, res);
+  if (url.startsWith("/users")) return userRoutes(req, res);
 }
 
 module.exports = router;
