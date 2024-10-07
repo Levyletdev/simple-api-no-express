20771658fb3990ef0fc8172d89cd634dfe90edc9
Levy Silva <levysillva@gmail.com>
2024-10-07 13:48:13 -0300

    refactor: integrate Habibiltado ObjectValue into User entity

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -1,13 +1,14 @@
 const Nome = require("../object-values/Nome.js");
 const Idade = require("../object-values/Idade.js");
 const Genero = require("../object-values/Genero.js");
+const Habilitado = require("../object-values/Habilitado.js");
 
 class User {
   constructor({ nome, idade, genero, habilitado }) {
     this.nome = new Nome(nome);
     this.idade = new Idade(idade);
     this.genero = new Genero(genero);
-    this.habilitado = habilitado;
+    this.habilitado = new Habilitado(habilitado, idade);
   }
 }
 
