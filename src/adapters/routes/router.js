f4e9644667e92ddbb514a05f95f2a2d76ce8feae
Levy Silva <levysillva@gmail.com>
2024-10-07 16:02:37 -0300

    refactor: import errorHandler and add it as third parameter for route hanfling

--- src/adapters/routes/router.js
+++ src/adapters/routes/router.js
@@ -1,10 +1,11 @@
 const rootRoute = require("./rootRoute.js");
 const userRoutes = require("./userRoutes.js");
+const errorHandler = require("../../utils/handlers/errorHandler.js");
 
-function router(req, res) {
+function router(req, res, handler = errorHandler) {
   const { url } = req;
   if (url === "/") return rootRoute(req, res);
-  if (url.startsWith("/users")) return userRoutes(req, res);
+  if (url.startsWith("/users")) return userRoutes(req, res, handler);
 }
 
 module.exports = router;
