a939e58e9145242e7d8f11741ba3726be1b77d1d
Levy Silva <levysillva@gmail.com>
2024-10-07 14:57:31 -0300

    feat: create ErrorBase class for custom error handling

---
+++ src/utils/errors/ErrorBase.js
@@ -0,0 +1,17 @@
+const Responser = require("../helpers/Responser.js");
+
+class ErrorBase extends Error {
+  constructor(message = "Erro interno do servidor", status = 500) {
+    this.message = message;
+    this.status = status;
+  }
+
+  send(res) {
+    return new Responser(res).status(this.status).json({
+      message: this.message,
+      status: this.status,
+    });
+  }
+}
+
+module.exports = ErrorBase;
