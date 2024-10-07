d5a18419ee2f25516e267289ae441b1a4852b8fd
Levy Silva <levysillva@gmail.com>
2024-10-07 16:17:59 -0300

    fix: call missing super constructor in ErrorBase

--- src/utils/errors/ErrorBase.js
+++ src/utils/errors/ErrorBase.js
@@ -2,6 +2,7 @@ const Responser = require("../helpers/Responser.js");
 
 class ErrorBase extends Error {
   constructor(message = "Erro interno do servidor", status = 500) {
+    super();
     this.message = message;
     this.status = status;
   }
