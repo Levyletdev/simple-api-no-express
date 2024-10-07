db768531dafcb2bbb90b92fa92a90a1de6ece5cc
Levy Silva <levysillva@gmail.com>
2024-10-07 00:20:35 -0300

    refactor: remove validateIdade method in User entity

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -16,40 +16,6 @@ class User {
     this.habilitado = habilitado;
   }
 
-  validateIdade(idade) {
-    if (isUndefined(idade)) {
-      throw new ValidationError({
-        name: "MissingPropertyError",
-        message: "Idade é obrigatória",
-        value: null,
-      });
-    }
-
-    if (notNumber(idade)) {
-      throw new ValidationError({
-        name: "InverseTypeError",
-        message: "Idade dever ser do tipo String",
-        value: idade,
-      });
-    }
-
-    if (isNumber(idade) && idade < 18) {
-      throw new ValidationError({
-        name: "InvalidDefinitionError",
-        message: "A idade deve ser maior ou igual a 18",
-        value: idade,
-      });
-    }
-
-    if (isNumber(idade) && idade > 100) {
-      throw new ValidationError({
-        name: "InvalidDefinitionError",
-        message: "A idade deve ser menor que ou igual a 100",
-        value: idade,
-      });
-    }
-  }
-
   validateGenero(genero) {
     if (isUndefined(genero)) {
       throw new ValidationError({
