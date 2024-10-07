193c0d3529c7d701a432068386623bdb6b2f9816
Levy Silva <levysillva@gmail.com>
2024-10-07 14:33:00 -0300

    refactor: remove getNome method call from constructor

--- src/domain/object-values/Nome.js
+++ src/domain/object-values/Nome.js
@@ -7,7 +7,6 @@ const ValidationError = require("../../utils/helpers/ValidationError.js");
 class Nome {
   constructor(nome) {
     this.validateNome(nome);
-    this.getNome();
   }
 
   validateNome(nome) {
