f7b1dba932fa47a6d611dd9ea2d4f826280a833c
Levy Silva <levysillva@gmail.com>
2024-10-07 00:43:41 -0300

    refactor: remove unused imports after ObjectValues migration in User entity

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -1,13 +1,5 @@
 const Nome = require("../object-values/Nome.js");
 const Idade = require("../object-values/Idade.js");
-const notIn = require("../../utils/validators/notIn.js");
-const isNumber = require("../../utils/validators/isNumber.js");
-const isString = require("../../utils/validators/isString.js");
-const notAlpha = require("../../utils/validators/notAlpha.js");
-const notNumber = require("../../utils/validators/notNumber.js");
-const notString = require("../../utils/validators/notString.js");
-const isUndefined = require("../../utils/validators/isUndefined.js");
-const ValidationError = require("../../utils/helpers/ValidationError.js");
 const Genero = require("../object-values/Genero.js");
 
 class User {
