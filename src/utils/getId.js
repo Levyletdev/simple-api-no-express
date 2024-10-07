ce2b47f504ac3677a0659b3eab02a92ca3e70d4b
Levy Silva <levysillva@gmail.com>
2024-10-06 21:48:33 -0300

    delete: remove duplicate files

--- src/utils/getId.js
+++
@@ -1,9 +0,0 @@
-const getParams = require("./getParams.js");
-
-function getId(req) {
-  const [endpoint, id] = getParams(req);
-  if (id === "undefined") return null;
-  return id;
-}
-
-module.exports = getId;
