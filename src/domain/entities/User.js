9b8f1f8c847a6038b49608a3bd7428ce57d75314
Levy Silva <levysillva@gmail.com>
2024-10-06 22:57:31 -0300

    fix: update import paths for 'User.js' after directory changes

--- src/domain/entities/User.js
+++ src/domain/entities/User.js
@@ -1,11 +1,11 @@
-const notIn = require("../utils/validators/notIn.js");
-const isNumber = require("../utils/validators/isNumber.js");
-const isString = require("../utils/validators/isString.js");
-const notAlpha = require("../utils/validators/notAlpha.js");
-const notNumber = require("../utils/validators/notNumber.js");
-const notString = require("../utils/validators/notString.js");
-const isUndefined = require("../utils/validators/isUndefined.js");
-const ValidationError = require("../utils/helpers/ValidationError.js");
+const notIn = require("../../utils/validators/notIn.js");
+const isNumber = require("../../utils/validators/isNumber.js");
+const isString = require("../../utils/validators/isString.js");
+const notAlpha = require("../../utils/validators/notAlpha.js");
+const notNumber = require("../../utils/validators/notNumber.js");
+const notString = require("../../utils/validators/notString.js");
+const isUndefined = require("../../utils/validators/isUndefined.js");
+const ValidationError = require("../../utils/helpers/ValidationError.js");
 
 class User {
   constructor({ nome, idade, genero, habilitado }) {
