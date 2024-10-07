4da33d574b842602a360c94d2a7cc363e6198017
Levy Silva <levysillva@gmail.com>
2024-10-07 19:42:10 -0300

    fix: change default port number to 3200

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv/config");
 const app = require("./src/app.js");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => console.log(`Server ouvindo na porta ${PORT}`));
