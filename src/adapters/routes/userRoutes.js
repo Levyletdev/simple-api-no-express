59e91cc570252668f184ecddbe5f86d4b2d5c7ac
Levy Silva <levysillva@gmail.com>
2024-10-07 16:08:00 -0300

    refactor: implement handler as third parameter in userRoutes

--- src/adapters/routes/userRoutes.js
+++ src/adapters/routes/userRoutes.js
@@ -7,15 +7,15 @@ const userRepository = new UserRepository();
 const userUseCase = new UserUseCase(userRepository);
 const userController = new UserController(userUseCase);
 
-function userRoutes(req, res) {
+function userRoutes(req, res, handler) {
   const { url, method } = req;
   const [endpoint, id] = getParams(req);
   const route = "/users";
 
-  if (url === route && method === "GET") return userController.getAll(req, res);
-  if (url === route && method === "POST") return userController.createUser(req, res);
-  if (endpoint === "users" && method === "GET") return userController.getById(req, res);
-  if (endpoint === "users" && method === "DELETE") return userController.deleteById(req, res);
+  if (url === route && method === "GET") return userController.getAll(req, res, handler);
+  if (url === route && method === "POST") return userController.createUser(req, res, handler);
+  if (endpoint === "users" && method === "GET") return userController.getById(req, res, handler);
+  if (endpoint === "users" && method === "DELETE") return userController.deleteById(req, res, handler);
 }
 
 module.exports = userRoutes;
