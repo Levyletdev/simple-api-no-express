7cbfeaf6242940379a675fe0e2dc8b2746d404bc
Levy Silva <levysillva@gmail.com>
2024-10-09 15:08:48 -0300

    feat: add notFoundHandler function as fourth parameter in router

--- src/adapters/routes/router.js
+++ src/adapters/routes/router.js
@@ -1,11 +1,13 @@
 const rootRoute = require("./rootRoute.js");
 const userRoutes = require("./userRoutes.js");
 const errorHandler = require("../../utils/handlers/errorHandler.js");
+const notFoundHandler = require("../../utils/handlers/notFoundHanler.js");
 
-function router(req, res, handler = errorHandler) {
+function router(req, res, handler = errorHandler, reject = notFoundHandler) {
   const { url } = req;
-  if (url === "/") return rootRoute(req, res);
-  if (url.startsWith("/users")) return userRoutes(req, res, handler);
+  if (url === "/") return rootRoute(req, res, handler, reject);
+  if (url.startsWith("/users")) return userRoutes(req, res, handler, reject);
+  reject(res);
 }
 
 module.exports = router;
