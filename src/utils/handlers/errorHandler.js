ffb722b32dddd8c74b2e31c9ae08f84b631df4ec
Levy Silva <levysillva@gmail.com>
2024-10-09 15:02:05 -0300

    feat: update errorHandler to check if error is instance of NotFoundError

--- src/utils/handlers/errorHandler.js
+++ src/utils/handlers/errorHandler.js
@@ -1,9 +1,12 @@
 const ServerError = require("../errors/ServerError.js");
+const NotFoundError = require("../errors/notFoundError.js");
 const ValidationError = require("../helpers/ValidationError.js");
 
 function errorHandler(error, res) {
   if (error instanceof ValidationError) {
     return error.report(res);
+  } else if (error instanceof NotFoundError) {
+    return error.send(res);
   } else {
     return new ServerError(error).send(res);
   }
