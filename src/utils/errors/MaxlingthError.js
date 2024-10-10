4eb13cfc3991b28cdfacfabe30af068391522d11
Levy Silva <levysillva@gmail.com>
2024-10-10 12:15:19 -0300

    delete: remove MaxlingthError

--- src/utils/errors/MaxlingthError.js
+++
@@ -1,8 +0,0 @@
-class MaxlengthError extends Error {
-  constructor(value, max) {
-    super();
-    this.message = `${value} must be no more than ${max} characters long.`;
-  }
-}
-
-module.exports = MaxlengthError;
