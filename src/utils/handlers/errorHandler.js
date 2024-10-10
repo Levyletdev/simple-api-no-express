01b5dba05bf2542bda5743aa66f8d8de031e5c20
Levy Silva <levysillva@gmail.com>
2024-10-10 15:42:59 -0300

    style: reorganize validations

--- src/utils/handlers/errorHandler.js
+++ src/utils/handlers/errorHandler.js
@@ -3,13 +3,9 @@ const NotFoundError = require("../errors/notFoundError.js");
 const ValidationError = require("../helpers/ValidationError.js");
 
 function errorHandler(error, res) {
-  if (error instanceof ValidationError) {
-    return error.report(res);
-  } else if (error instanceof NotFoundError) {
-    return error.send(res);
-  } else {
-    return new ServerError(error).send(res);
-  }
+  if (error instanceof ValidationError) return error.report(res);
+  if (error instanceof NotFoundError) return error.send(res);
+  return new ServerError(error).send(res);
 }
 
 module.exports = errorHandler;
